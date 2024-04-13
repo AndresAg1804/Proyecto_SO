56e2362aa35847b0278b3a03ee0f0bd7bf31a3fc
AndresAg1804 <anner.angulo.gutierrez@est.una.ac.cr>
2024-04-12 22:35:07 -0600

    Avance en el proceso CPU

--- procesoCPU.c
+++ procesoCPU.c
@@ -1,57 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/types.h>
 
-int main() {
-    FILE *fp;
-    char buffer[1024];
-    long double a[4], b[4], loadavg;
-    const char* sep = " \t\n";
+//No funciona Bien. Este es el del Porcentaje de utilizacion de un proceso en 5 minutos
+double get_cpu_usage(pid_t pid) {
+    struct rusage usage;
+    struct timeval start, end;
+    double total_time, cpu_time;
 
-    fp = fopen("/proc/stat", "r");
+    gettimeofday(&start, NULL);
+    if (getrusage(RUSAGE_CHILDREN, &usage) == -1) {
+        perror("getrusage");
+        exit(EXIT_FAILURE);
+    }
+    total_time = usage.ru_utime.tv_sec + usage.ru_stime.tv_sec +
+                 (usage.ru_utime.tv_usec + usage.ru_stime.tv_usec) / 1000000.0;
+
+    sleep(5 * 60); // wait for 5 minutes
+
+    gettimeofday(&end, NULL);
+    if (getrusage(RUSAGE_CHILDREN, &usage) == -1) {
+        perror("getrusage");
+        exit(EXIT_FAILURE);
+    }
+    total_time = end.tv_sec - start.tv_sec + (end.tv_usec - start.tv_usec) / 1000000.0;
+    cpu_time = usage.ru_utime.tv_sec + usage.ru_stime.tv_sec +
+               (usage.ru_utime.tv_usec + usage.ru_stime.tv_usec) / 1000000.0;
+
+    return (cpu_time / total_time) * 100.0;
+}
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    char cmd[256];
+    struct stat st;
+    double usage;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <PID>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    pid = atoi(argv[1]);
+
+    if (stat(argv[1], &st) == -1 || !S_ISREG(st.st_mode)) {
+        fprintf(stderr, "%s is not a valid PID\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    usage = get_cpu_usage(pid);
+
+    if (usage < 0.0) {
+        fprintf(stderr, "Error: could not get CPU usage for PID %d\n", pid);
+        exit(EXIT_FAILURE);
+    }
+
+    snprintf(cmd, sizeof(cmd), "/proc/%d/status", pid);
+    FILE *fp = fopen(cmd, "r");
     if (fp == NULL) {
-        printf("No se pudo abrir el archivo /proc/stat\n");
-        exit(1);
+        perror("fopen");
+        exit(EXIT_FAILURE);
     }
 
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        if (strstr(buffer, "cpu")) {
-            char* token = strtok(buffer, sep);
-            for(int i = 0; i < 4; i++) {
-                token = strtok(NULL, sep);
-                a[i] = strtold(token, NULL);
-            }
-            break;
-        }
-    }
-
-    fclose(fp);
-    sleep(1);
-
-    fp = fopen("/proc/stat", "r");
-    if (fp == NULL) {
-        printf("No se pudo abrir el archivo /proc/stat\n");
-        exit(1);
-    }
-
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        if (strstr(buffer, "cpu")) {
-            char* token = strtok(buffer, sep);
-            for(int i = 0; i < 4; i++) {
-                token = strtok(NULL, sep);
-                b[i] = strtold(token, NULL);
-            }
-            break;
-        }
-    }
-
+    char name[16];
+    fgets(name, sizeof(name), fp);
     fclose(fp);
 
-    loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]));
-    loadavg *= 100;
+    name[strcspn(name, "\n")] = '\0';
 
-    printf("CPU utilization: %.2Lf%%\n", loadavg);
+    printf("Process %s (PID %d) CPU usage: %.2f%%\n", name, pid, usage);
 
     return 0;
 }
